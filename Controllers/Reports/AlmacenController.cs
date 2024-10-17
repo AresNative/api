ab2c298f8a959da4a50ebf666a10a530546e8ab7
Juan José Eusebio <juanerpgr@gmail.com>
2024-10-17 12:56:46 -0700

    ajuste de edpoints

--- Controllers/Reports/AlmacenController.cs
+++ Controllers/Reports/AlmacenController.cs
@@ -38,46 +38,24 @@ namespace MyApiProject.Controllers
                 }
 
                 await using var reader = await command.ExecuteReaderAsync();
-
-                var almacen = new List<AlmacenDto>(); // Lista donde se almacenarán los resultados
+                var results = new List<Dictionary<string, object>>();
 
                 while (await reader.ReadAsync())
                 {
-                    almacen.Add(MapToAlmacenDto(reader)); // Usamos el método correcto para mapear los datos
+                    var row = new Dictionary<string, object>();
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        row[reader.GetName(i)] = reader.GetValue(i);
+                    }
+                    results.Add(row);
                 }
 
-                return Ok(almacen); // Retornamos los resultados en formato JSON
+                return Ok(results);
             }
             catch (Exception ex)
             {
                 return HandleException(ex); // Método para gestionar las excepciones
             }
-        }
-
-        private AlmacenDto MapToAlmacenDto(SqlDataReader reader)
-        {
-            return new AlmacenDto
-            {
-                Id = reader.IsDBNull(reader.GetOrdinal("id")) ? 0 : reader.GetInt32(reader.GetOrdinal("id")),
-                IdTypeTaxes = reader.IsDBNull(reader.GetOrdinal("id_type_taxes")) ? null : reader.GetString(reader.GetOrdinal("id_type_taxes")),
-                IdMov = reader.IsDBNull(reader.GetOrdinal("id_mov")) ? null : reader.GetString(reader.GetOrdinal("id_mov")),
-                IdState = reader.IsDBNull(reader.GetOrdinal("id_state")) ? null : reader.GetString(reader.GetOrdinal("id_state")),
-                IdCaja = reader.IsDBNull(reader.GetOrdinal("id_caja")) ? null : reader.GetString(reader.GetOrdinal("id_caja")),
-                IdTypePago = reader.IsDBNull(reader.GetOrdinal("id_type_pago")) ? null : reader.GetString(reader.GetOrdinal("id_type_pago")),
-                IdSucursal = reader.IsDBNull(reader.GetOrdinal("id_sucursal")) ? null : reader.GetString(reader.GetOrdinal("id_sucursal")),
-                IdAlmacen = reader.IsDBNull(reader.GetOrdinal("id_almacen")) ? null : reader.GetString(reader.GetOrdinal("id_almacen")),
-                Art = reader.IsDBNull(reader.GetOrdinal("art")) ? null : reader.GetString(reader.GetOrdinal("art")),
-                Cant = reader.IsDBNull(reader.GetOrdinal("cant")) ? 0 : reader.GetInt32(reader.GetOrdinal("cant")),
-                Price = reader.IsDBNull(reader.GetOrdinal("price")) ? 0 : reader.GetDecimal(reader.GetOrdinal("price")),
-                Taxes = reader.IsDBNull(reader.GetOrdinal("taxes")) ? null : reader.GetString(reader.GetOrdinal("taxes")),
-                Unit = reader.IsDBNull(reader.GetOrdinal("unit")) ? null : reader.GetString(reader.GetOrdinal("unit")),
-                StateArt = reader.IsDBNull(reader.GetOrdinal("state_art")) ? null : reader.GetString(reader.GetOrdinal("state_art")),
-                User = reader.IsDBNull(reader.GetOrdinal("user")) ? null : reader.GetString(reader.GetOrdinal("user")),
-                Client = reader.IsDBNull(reader.GetOrdinal("client")) ? null : reader.GetString(reader.GetOrdinal("client")),
-                Pago = reader.IsDBNull(reader.GetOrdinal("pago")) ? 0 : reader.GetDecimal(reader.GetOrdinal("pago")),
-                Import = reader.IsDBNull(reader.GetOrdinal("import")) ? 0 : reader.GetDecimal(reader.GetOrdinal("import")),
-                Currency = reader.IsDBNull(reader.GetOrdinal("currency")) ? null : reader.GetString(reader.GetOrdinal("currency"))
-            };
         }
     }
 }
